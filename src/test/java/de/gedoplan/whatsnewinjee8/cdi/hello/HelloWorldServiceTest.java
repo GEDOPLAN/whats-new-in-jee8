fb5554e5fe685765fed336beb8d7291185e3b034
Dirk Weil <dirk.weil@gedoplan.de>
2017-11-17 19:05:01 +0100

    .

--- src/test/java/de/gedoplan/whatsnewinjee8/cdi/hello/HelloWorldServiceTest.java
+++ src/test/java/de/gedoplan/whatsnewinjee8/cdi/hello/HelloWorldServiceTest.java
@@ -1,9 +1,11 @@
 package de.gedoplan.whatsnewinjee8.cdi.hello;
 
+import de.gedoplan.baselibs.utils.util.ClassUtil;
 import de.gedoplan.whatsnewinjee8.cdi.CdiTestBase;
 
 import javax.inject.Inject;
 
+import org.apache.commons.logging.Log;
 import org.junit.Test;
 
 public class HelloWorldServiceTest extends CdiTestBase {
@@ -11,8 +13,14 @@ public class HelloWorldServiceTest extends CdiTestBase {
   @Inject
   HelloWorldService helloWorldService;
 
+  @Inject
+  Log log;
+
   @Test
   public void testHelloWorld() throws Exception {
-    System.out.println(this.helloWorldService.getHelloWorld());
+    this.log.debug(this.helloWorldService.getHelloWorld());
+
+    this.log.debug("Class:         " + this.helloWorldService.getClass());
+    this.log.debug("Proxied class: " + ClassUtil.getProxiedClass(this.helloWorldService.getClass()));
   }
 }

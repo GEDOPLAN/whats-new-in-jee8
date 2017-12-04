7ec6f59f4771eb8a76c94b2f12b79736e3f12197
Dirk Weil <dirk.weil@gedoplan.de>
2017-12-04 16:04:30 +0100

    .

--- src/main/java/de/gedoplan/whatsnewinjee8/jsf/ZonedDateTimeConverter.java
+++ src/main/java/de/gedoplan/whatsnewinjee8/jsf/ZonedDateTimeConverter.java
@@ -2,6 +2,7 @@ package de.gedoplan.whatsnewinjee8.jsf;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
@@ -11,18 +12,18 @@ import javax.faces.convert.FacesConverter;
 @FacesConverter(forClass = ZonedDateTime.class)
 public class ZonedDateTimeConverter implements Converter<ZonedDateTime> {
 
-    @Override
-    public ZonedDateTime getAsObject(FacesContext context, UIComponent component, String value) {
-        try {
-            return value == null ? null : ZonedDateTime.parse(value, DateTimeFormatter.ISO_ZONED_DATE_TIME);
-        } catch (Exception e) {
-            throw new ConverterException(e);
-        }
+  @Override
+  public ZonedDateTime getAsObject(FacesContext context, UIComponent component, String value) {
+    try {
+      return value == null ? null : ZonedDateTime.parse(value, DateTimeFormatter.ISO_ZONED_DATE_TIME);
+    } catch (Exception e) {
+      throw new ConverterException(e);
     }
+  }
 
-    @Override
-    public String getAsString(FacesContext context, UIComponent component, ZonedDateTime value) {
-        return value == null ? null : value.format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
-    }
+  @Override
+  public String getAsString(FacesContext context, UIComponent component, ZonedDateTime value) {
+    return value == null ? null : value.format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
+  }
 
 }
